d8cc537fbcc30f053e2a748099521e4640bc5203
Ebabil <Ebabil@DESKTOP-6QVU3C3>
2025-05-12 20:04:20 +0300

    Add project files.

---
+++ GorselProgramlamaProje/AnaSayfa.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace GorselProgramlamaProje
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaSayfa));
+            this.btn_grs = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.khvlt_pic = new System.Windows.Forms.PictureBox();
+            this.tatli_pic = new System.Windows.Forms.PictureBox();
+            this.atstr_pic = new System.Windows.Forms.PictureBox();
+            this.anaymk_pic = new System.Windows.Forms.PictureBox();
+            this.sck_pic = new System.Windows.Forms.PictureBox();
+            this.sgk_pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.khvlt_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tatli_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atstr_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anaymk_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sck_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sgk_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_grs
+            // 
+            this.btn_grs.BackColor = System.Drawing.Color.Tan;
+            this.btn_grs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_grs.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_grs.Location = new System.Drawing.Point(343, 12);
+            this.btn_grs.Name = "btn_grs";
+            this.btn_grs.Size = new System.Drawing.Size(145, 26);
+            this.btn_grs.TabIndex = 0;
+            this.btn_grs.Text = "Yönetici/Personel";
+            this.btn_grs.UseVisualStyleBackColor = false;
+            this.btn_grs.Click += new System.EventHandler(this.btn_grs_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Campana Script PERSONAL USE", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(37, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 94);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KAFE ADI";
+            // 
+            // khvlt_pic
+            // 
+            this.khvlt_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.khvlt;
+            this.khvlt_pic.Location = new System.Drawing.Point(36, 125);
+            this.khvlt_pic.Name = "khvlt_pic";
+            this.khvlt_pic.Size = new System.Drawing.Size(190, 140);
+            this.khvlt_pic.TabIndex = 2;
+            this.khvlt_pic.TabStop = false;
+            // 
+            // tatli_pic
+            // 
+            this.tatli_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.tatli;
+            this.tatli_pic.Location = new System.Drawing.Point(272, 125);
+            this.tatli_pic.Name = "tatli_pic";
+            this.tatli_pic.Size = new System.Drawing.Size(190, 140);
+            this.tatli_pic.TabIndex = 3;
+            this.tatli_pic.TabStop = false;
+            // 
+            // atstr_pic
+            // 
+            this.atstr_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.atstr;
+            this.atstr_pic.Location = new System.Drawing.Point(273, 302);
+            this.atstr_pic.Name = "atstr_pic";
+            this.atstr_pic.Size = new System.Drawing.Size(190, 140);
+            this.atstr_pic.TabIndex = 5;
+            this.atstr_pic.TabStop = false;
+            // 
+            // anaymk_pic
+            // 
+            this.anaymk_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.anaymk;
+            this.anaymk_pic.Location = new System.Drawing.Point(37, 302);
+            this.anaymk_pic.Name = "anaymk_pic";
+            this.anaymk_pic.Size = new System.Drawing.Size(190, 140);
+            this.anaymk_pic.TabIndex = 4;
+            this.anaymk_pic.TabStop = false;
+            // 
+            // sck_pic
+            // 
+            this.sck_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.sck;
+            this.sck_pic.Location = new System.Drawing.Point(273, 477);
+            this.sck_pic.Name = "sck_pic";
+            this.sck_pic.Size = new System.Drawing.Size(190, 140);
+            this.sck_pic.TabIndex = 7;
+            this.sck_pic.TabStop = false;
+            // 
+            // sgk_pic
+            // 
+            this.sgk_pic.BackgroundImage = global::GorselProgramlamaProje.Properties.Resources.sgk;
+            this.sgk_pic.Location = new System.Drawing.Point(37, 477);
+            this.sgk_pic.Name = "sgk_pic";
+            this.sgk_pic.Size = new System.Drawing.Size(190, 140);
+            this.sgk_pic.TabIndex = 6;
+            this.sgk_pic.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(146, 638);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 14);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Bizi değerlendirmek ister misiniz?";
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(500, 666);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_grs);
+            this.Controls.Add(this.sck_pic);
+            this.Controls.Add(this.sgk_pic);
+            this.Controls.Add(this.atstr_pic);
+            this.Controls.Add(this.anaymk_pic);
+            this.Controls.Add(this.tatli_pic);
+            this.Controls.Add(this.khvlt_pic);
+            this.Controls.Add(this.label1);
+            this.Name = "AnaSayfa";
+            this.Text = "AnaSayfa";
+            this.Load += new System.EventHandler(this.AnaSayfa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.khvlt_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tatli_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atstr_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.anaymk_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sck_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sgk_pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_grs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox khvlt_pic;
+        private System.Windows.Forms.PictureBox tatli_pic;
+        private System.Windows.Forms.PictureBox atstr_pic;
+        private System.Windows.Forms.PictureBox anaymk_pic;
+        private System.Windows.Forms.PictureBox sck_pic;
+        private System.Windows.Forms.PictureBox sgk_pic;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GorselProgramlamaProje/AnaSayfa.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorselProgramlamaProje
+{
+    public partial class AnaSayfa : Form
+    {
+        private void trans_panel(PictureBox pictureBox, string text) {
+            // Yarı saydam panel oluştur
+            Panel overlay = new Panel
+            {
+                BackColor = Color.FromArgb(95, 0, 0, 0), // 128 = şeffaflık
+                Location = new Point(0, 0),
+                Size = pictureBox.Size
+
+            };
+
+            //Yazı (label ile)
+            Label label = new Label
+            {
+                Text = text,
+                ForeColor = Color.White,
+                BackColor = Color.Transparent,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                
+            };
+            overlay.Controls.Add(label);
+            pictureBox.Controls.Add(overlay);
+            overlay.BringToFront();
+        }
+            
+            
+
+        
+
+
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_grs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AnaSayfa_Load(object sender, EventArgs e)
+        {
+
+
+            // PictureBox üzerine ekle
+            trans_panel(khvlt_pic, "KAHVALTI");
+            trans_panel(atstr_pic, "ATIŞTIRMALIKLAR");
+            trans_panel(anaymk_pic, "ANA YEMEK");
+            trans_panel(tatli_pic, "TATLI");
+            trans_panel(sck_pic,"SICAK İÇECEKLER");
+            trans_panel(sgk_pic, "SOĞUK İÇECEKLER");
+        }
+    }
+}
