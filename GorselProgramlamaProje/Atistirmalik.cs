58bfd19f6e55ee842e3567261cdc96ab122a23ef
EbabilKvlcm <77108748+EbabilKvlcm@users.noreply.github.com>
2025-05-15 19:18:31 +0300

    Add files via upload

--- GorselProgramlamaProje/Atistirmalik.cs
+++ GorselProgramlamaProje/Atistirmalik.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Configuration;
+using MySql.Data.MySqlClient;
 namespace GorselProgramlamaProje
 {
     public partial class Atistirmalik : Form
--- GorselProgramlamaProje/SicakIcecek.cs
+++ GorselProgramlamaProje/SicakIcecek.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Configuration;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
